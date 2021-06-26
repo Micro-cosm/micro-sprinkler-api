20ecdc82fe50f74067356504046501cfaeb91c09
wejafoo <wes@weja.us>
2021-06-26 14:41:08 -0600

    Add new mcu mac address to request origins   * Update gitignore   * Clean up default log format

--- main.go
+++ main.go
@@ -16,20 +16,19 @@ import (
 )
 
 var (
-	debug     = false
+	isDebug   = false
 	returnSet = "{}"
-	safeList  = [4]string{
-		"00:00:00:00:00:00",
-		"E8:DB:84:97:BA:85",
-		"8C:AA:B5:C6:42:A6",
-		"EC:FA:BC:C0:AB:B1",
+	safeList  = [5]string{
+		"00:00:00:00:00:00", // postman test value
+		"E8:DB:84:97:BA:85", // esp8266-01-1
+		"8C:AA:B5:C6:42:A6", // esp8266-01-2
+		"EC:FA:BC:C0:AB:B1", // esp8266-12e-1
+		"2C:F4:32:14:6D:4C", // esp8266-12e-2
 	}
-
 	pollDuration time.Duration = 8
 )
 
 func pullMsgsSync(podNo string) error {
-
 	projectId := "weja-us"
 	subID := "pod" + podNo
 	ctx := context.Background()
@@ -49,7 +48,7 @@ func pullMsgsSync(podNo string) error {
 	sub.ReceiveSettings.Synchronous = true // Pull, in place of StreamingPull, will respect MaxOutstandingMessages
 	sub.ReceiveSettings.MaxOutstandingMessages = 1
 
-	log.Printf("Subscription request: pod%s", podNo)
+	log.Printf("request from: pod%s", podNo)
 
 	ctx, cancel := context.WithTimeout(ctx, pollDuration*time.Second) // Receive messages
 	defer cancel()
@@ -57,7 +56,9 @@ func pullMsgsSync(podNo string) error {
 	defer close(cm)
 
 	go func() { // goroutine iterates against next un-ack'd message
-		log.Printf("\tprocessing unread messsages...")
+		if isDebug {
+			log.Printf("\tprocessing unread messsages...")
+		}
 		count := 0
 		for msg := range cm {
 			count++
@@ -66,12 +67,10 @@ func pullMsgsSync(podNo string) error {
 			returnSet = string(msg.Data)
 		}
 	}()
-
 	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) { cm <- msg }) // Receive blocks until context is exhausted
 	if err != nil && status.Code(err) != codes.Canceled {
 		return fmt.Errorf("receive %v", err)
 	}
-
 	return nil
 }
 
@@ -84,70 +83,68 @@ func slContains(device string) bool {
 	return false
 }
 
-// Todo: Replace pod1 & pod2 with polling support for 1..n pod(s)
 func pod1(w http.ResponseWriter, r *http.Request) {
-
 	device, ok := r.URL.Query()["device"]
 	referer := r.Header.Get("referer")
 
 	if ok && slContains(device[0]) && device[0] == referer {
-
 		returnSet = "{}"
 		if err := pullMsgsSync("1"); err != nil {
 			return
 		}
 
-		log.Printf("\ttarget device: %s\n\t\t\ttarget intruction set:\n%s\n", referer, returnSet)
+		if isDebug {
+			log.Printf("\ttarget device: %s\n\t\t\ttarget instruction set:\n%s\n", referer, returnSet)
+		} else {
+			log.Printf("%s", returnSet)
+		}
+
 		fprintf, err := fmt.Fprintf(w, returnSet)
 		if err != nil {
 			log.Println(fprintf)
-		} // DETERMINE IF "return" SHOULD BE EXPLICIT HERE
+		}
 	} else {
-
 		log.Println("invalid device id in request... ignored.")
 		for name, values := range r.Header {
 			for _, value := range values {
 				log.Println(name, value)
 			}
-		} // Log all headers for invalid requests
+		}
 		msg := "{\"msg\":\"Sorry, can't help you :(\"}"
 		fprintf, err := fmt.Fprintf(w, "%s", msg)
 		if err != nil {
 			log.Println(fprintf)
-		} // DETERMINE IF "return" SHOULD BE EXPLICIT HERE
+		}
 	}
 }
 
 func pod2(w http.ResponseWriter, r *http.Request) {
-
 	device, ok := r.URL.Query()["device"]
 	referer := r.Header.Get("referer")
-
 	if ok && slContains(device[0]) && device[0] == referer {
-
 		returnSet = "{}"
 		if err := pullMsgsSync("2"); err != nil {
 			return
 		}
-
-		log.Printf("\ttarget device: %s\ttarget intruction set:\n%s", referer, returnSet)
+		if isDebug {
+			log.Printf("\ttarget device: %s\ttarget intruction set:\n%s", referer, returnSet)
+		}
 		fprintf, err := fmt.Fprintf(w, returnSet)
 		if err != nil {
 			log.Println(fprintf)
-		} // DETERMINE IF "return" SHOULD BE EXPLICIT HERE
+		}
 	} else {
-
 		log.Println("invalid device id in request... ignored.")
 		for name, values := range r.Header {
 			for _, value := range values {
 				log.Println(name, value)
 			}
-		} // Log all headers for invalid requests
+		}
 		msg := "{\"msg\":\"Sorry, can't help you :(\"}"
 		fprintf, err := fmt.Fprintf(w, "%s", msg)
 		if err != nil {
 			log.Println(fprintf)
-		} // DETERMINE IF "return" SHOULD BE EXPLICIT HERE
+		}
 	}
 }
 
@@ -160,26 +157,23 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func commonMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Add("Content-Type", "application/json")
-			next.ServeHTTP(w, r)
-		},
-	)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-	debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	isDebug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
 	routeBase := os.Getenv("ROUTE_BASE")
-
-	log.Println("Sparking up server on port: 8080(possibly mapped)", routeBase)
-
+	if isDebug {
+		log.Println("Sparking up server on port: 8080(possibly mapped)", routeBase)
+	}
 	var router = mux.NewRouter()
 	router.Use(commonMiddleware)
 	router.HandleFunc(routeBase+"1", pod1).Methods("GET")
 	router.HandleFunc(routeBase+"2", pod2).Methods("GET")
 	router.HandleFunc(routeBase, index).Methods("GET")
-
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
 	}
